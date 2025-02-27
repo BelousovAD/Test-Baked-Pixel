1ff5905364af81f54228ffec1e5198c3583489a3
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2025-02-27 17:03:50 +0700

    Созданы скрипты предметов

---
+++ Assets/Features/Items/Scripts/Data/AmmoData.cs
@@ -0,0 +1,11 @@
+﻿namespace TestBakedPixel.Data
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Данные патрона
+    /// </summary>
+    [CreateAssetMenu(fileName = nameof(AmmoData), menuName = "TestBakedPixel/Features/Data/New " + nameof(AmmoData))]
+    public class AmmoData : AbstractItemData
+    { }
+}
---
+++ Assets/Features/Items/Scripts/Data/HelmetData.cs
@@ -0,0 +1,22 @@
+﻿namespace TestBakedPixel.Data
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Данные шлема
+    /// </summary>
+    [CreateAssetMenu(fileName = nameof(HelmetData), menuName = "TestBakedPixel/Features/Data/New " + nameof(HelmetData))]
+    public class HelmetData : AbstractItemData
+    {
+        #region Properties
+
+        /// <summary>
+        /// Значение защиты
+        /// </summary>
+        public int ProtectionValue => _protectionValue;
+        [SerializeField, Min(0)]
+        private int _protectionValue = 1;
+
+        #endregion
+    }
+}
