57f0339ebfa66b8fdbc94096024ce1cba028d39b
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2025-02-28 03:23:03 +0700

    Создан скрипт данных о слоте

---
+++ Assets/Features/Slots/Scripts/Data/SlotData.cs
@@ -0,0 +1,30 @@
+﻿namespace TestBakedPixel.Slots.Data
+{
+    using System;
+    using UnityEngine;
+
+    /// <summary>
+    /// Данные о слоте
+    /// </summary>
+    [Serializable]
+    public class SlotData
+    {
+        #region Properties
+
+        /// <summary>
+        /// Заблокирован ли слот
+        /// </summary>
+        public bool IsLocked => _isLocked;
+        [SerializeField]
+        private bool _isLocked = false;
+
+        /// <summary>
+        /// Стоимость разблокировки слота
+        /// </summary>
+        public int CostToUnlock => _costToUnlock;
+        [SerializeField]
+        private int _costToUnlock = 0;
+
+        #endregion
+    }
+}
