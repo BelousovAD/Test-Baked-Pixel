ea04beb3141fc3226db45db5084295fc1e17154e
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2025-02-28 03:25:54 +0700

    Создан скрипт переключателя объектов

---
+++ Assets/Features/GameObjects/Scripts/ObjectSwitcher.cs
@@ -0,0 +1,30 @@
+﻿namespace TestBakedPixel.GameObjects
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// Переключатель объектов
+    /// </summary>
+    public class ObjectSwitcher : MonoBehaviour
+    {
+        #region Properties
+
+        [SerializeField]
+        private List<GameObject> objectsToEnable = new List<GameObject>();
+        [SerializeField]
+        private List<GameObject> objectsToDisable = new List<GameObject>();
+
+        #endregion
+
+        #region Methods
+
+        protected void SwitchObjects(bool status)
+        {
+            objectsToEnable.ForEach(x => x.SetActive(status));
+            objectsToDisable.ForEach(x => x.SetActive(!status));
+        }
+
+        #endregion
+    }
+}
