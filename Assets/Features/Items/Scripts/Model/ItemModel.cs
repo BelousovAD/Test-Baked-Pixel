71add38325d47ae9f8d144f636f00bb83bf22966
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2025-02-27 17:36:18 +0700

    Создан скрипт предмета в инвентаре

---
+++ Assets/Features/Items/Scripts/Model/ItemModel.cs
@@ -0,0 +1,32 @@
+﻿namespace TestBakedPixel.Items.Model
+{
+    using TestBakedPixel.Items.Data;
+
+    /// <summary>
+    /// Предмет в инвентаре
+    /// </summary>
+    public class ItemModel
+    {
+        #region Properties
+
+        /// <summary>
+        /// Количество предмета в стеке
+        /// </summary>
+        public int StackCount => _stackCount;
+        private int _stackCount = 1;
+
+        private AbstractItemData _itemData = default;
+
+        #endregion
+
+        #region Methods
+
+        public ItemModel(AbstractItemData itemData, int stackCount = 1)
+        {
+            _itemData = itemData;
+            _stackCount = stackCount;
+        }
+
+        #endregion
+    }
+}
