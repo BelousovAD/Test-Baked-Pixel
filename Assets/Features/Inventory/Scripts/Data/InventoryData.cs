b9ec1a390fc38fe5117aa4abe20e2004154834a0
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2025-02-28 03:23:51 +0700

    Создан скрипт и SO данных инвентаря

---
+++ Assets/Features/Inventory/Scripts/Data/InventoryData.cs
@@ -0,0 +1,24 @@
+﻿namespace TestBakedPixel.Inventory.Data
+{
+    using System.Collections.Generic;
+    using TestBakedPixel.Slots.Data;
+    using UnityEngine;
+
+    /// <summary>
+    /// Данные инвентаря
+    /// </summary>
+    [CreateAssetMenu(fileName = nameof(InventoryData), menuName = "TestBakedPixel/Features/Inventory/Data/New " + nameof(InventoryData))]
+    public class InventoryData : ScriptableObject
+    {
+        #region Properties
+
+        /// <summary>
+        /// Список данных слотов по умолчанию
+        /// </summary>
+        public List<SlotData> DefaultSlotDatas => _defaultSlotDatas;
+        [SerializeField]
+        private List<SlotData> _defaultSlotDatas = new List<SlotData>();
+
+        #endregion
+    }
+}
